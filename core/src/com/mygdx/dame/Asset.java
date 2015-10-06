a7441b9d24d48a8b5acad011e6af9837f4114a7f
Raldir <rami-aly@hotmail.de>
2015-10-06 22:52:03 +0200

    changed Asset inheritance

--- core/src/com/mygdx/dame/Asset.java
+++ core/src/com/mygdx/dame/Asset.java
@@ -1,23 +1,60 @@
 package com.mygdx.dame;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class Asset extends AbstractAsset{
+public abstract class AbstractAsset extends Image {
 
+	public Token token = null;
+	public int index;
 	
-	public Asset(String pfad, float x, float y, float width, float height, int index){
-		super(pfad, x ,y, width, height, index);
+	public AbstractAsset(String pfad, float x, float y, float width, float height, int index){
+		super(createTexture(pfad));
+		super.setBounds(x, y, width, height);
+		this.index = index;
+		
+		
 	}
 	
-	@Override
-	public boolean specialEvent() {
-		return false;
+	public abstract boolean specialEvent();
+	
+	public static Texture createTexture(String pfad){
+		Texture texture_back = new Texture(Gdx.files.internal(pfad));
+		texture_back.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		return texture_back;
 	}
 	
+	public void setIndex(int index){
+		this.index = index;
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+	
+	public void setToken(Token token){
+		this.token = token;
+	}
+	
+	public Token getToken(){
+		return token;
+	}
+	
+	public void removeToken(){
+		System.out.println(token.getX());
+		token.remove();
+		setToken(null);
+	}
+	
+	public void refreshToken(ArrayList<AbstractAsset> as){
+		if(token != null){
+			token.movement(as);
+		}
+	}
 }
+
+
