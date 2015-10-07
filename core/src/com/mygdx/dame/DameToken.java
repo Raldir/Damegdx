f7dc9506d7574b7d0ad6b83b7ab6f74b66bc4a04
Raldir <rami-aly@hotmail.de>
2015-10-07 20:51:01 +0200

    implementation of DameToken 0.1

--- core/src/com/mygdx/dame/DameToken.java
+++ core/src/com/mygdx/dame/DameToken.java
@@ -1,5 +1,7 @@
 package com.mygdx.dame;
 
+import java.util.ArrayList;
+
 
 public class DameToken extends Token {
 
@@ -7,5 +9,21 @@ public class DameToken extends Token {
 		super(pfad, as, p);
 		p.addToken(this);
 	}
-
+	
+	@Override
+	public void addTarget(final ArrayList<Asset> assets, final int target, Player p){
+		
+	}
+	
+	@Override
+	public void setGenerellMovement(Player p, final ArrayList<Asset> assets){
+		
+		
+	}
+	
+	@Override
+	public void movement(ArrayList<Asset> assets){
+		
+		
+	}
 }
